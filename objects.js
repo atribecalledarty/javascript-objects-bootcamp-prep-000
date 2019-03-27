8a1be62f2f16d1d8cfec58c32f6ed2bb5b25f1be
Arthur Song <arth3rs0ng@gmail.com>
2019-03-27 06:58:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ var playlist = {
 function updatePlaylist (playlist, artist, title){
   return Object.assign({}, playlist, {[artist]: title})
 }
+
+function removeFromPlaylist (playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
